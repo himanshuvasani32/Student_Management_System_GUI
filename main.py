6742f7609e330908889231a82f4330e669901eed
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-08-04 00:52:46 +0530

    Added Table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QGridLayout, QMainWindow
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QLineEdit, QGridLayout, QMainWindow,
+                             QTableWidget)
+
 from PyQt6.QtGui import QAction
 
 
@@ -18,6 +20,14 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        self.table
 
 
 app = QApplication(sys.argv)

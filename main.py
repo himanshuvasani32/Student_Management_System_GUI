7c4a8b1c02df225f44432b36d9b7e272d553599f
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-08-04 16:21:50 +0530

    Added about dialog and details for about

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+        about_action.triggered.connect(self.about)
 
         search_action = QAction(QIcon("icons/search.png"), "Search", self)
         edit_menu_item.addAction(search_action)
@@ -93,6 +94,21 @@ class MainWindow(QMainWindow):
     def delete(self):
         dialog = DeleteDialog()
         dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
+        dialog.exec()
+
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("About the app")
+        content = """
+        This app is for student record management and it has the add, edit, search and delete functionalities.
+        """
+        self.setText(content)
 
 
 class EditDialog(QDialog):
